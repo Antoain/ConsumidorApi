9b84ee3e0a69bb1b00eb9e53c98fa9cc81254a0e
Antony <126409554+Antoain@users.noreply.github.com>
2025-02-26 19:36:20 -0600

    Haciendo una cambiesillos

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 document.getElementById('getDataBtn').addEventListener('click', fetchChessData);
 
 function fetchChessData() {
-    fetch('https://api.chess.com/pub/player/hikaru')
+    const username = document.getElementById('username').value;
+    if (!username) {
+        document.getElementById('data').innerText = 'Por favor, introduce un nombre de usuario';
+        return;
+    }
+
+    fetch(`https://api.chess.com/pub/player/${username}`)
         .then(response => {
             if (!response.ok) {
-                throw new Error('hubo un error de red');
+                throw new Error('Hubo un error de red');
             }
             return response.json();
         })
         .then(playerData => {
-            fetch('https://api.chess.com/pub/player/hikaru/stats')
+            fetch(`https://api.chess.com/pub/player/${username}/stats`)
                 .then(response => {
                     if (!response.ok) {
                         throw new Error('Hubo un error de red');
@@ -18,17 +24,17 @@ function fetchChessData() {
                 })
                 .then(data => {
                     document.getElementById('data').innerHTML = `
-                        <img src="${playerData.avatar}" alt="Hikaru Nakamura">
+                        <img src="${playerData.avatar}" alt="${username}">
                         <p><strong>Nombre de Usuario:</strong> ${playerData.username}</p>
                         <p><strong>Blitz Rating:</strong> ${data.chess_blitz.last.rating}</p>
                         <p><strong>Rapid Rating:</strong> ${data.chess_rapid.last.rating}</p>
                     `;
                 })
                 .catch(error => {
-                    document.getElementById('data').innerText = 'no se pudieron obtener los datos: ' + error.message;
+                    document.getElementById('data').innerText = 'No se pudieron obtener los datos: ' + error.message;
                 });
         })
         .catch(error => {
-            document.getElementById('data').innerText = 'no se pudieron obtener los datos: ' + error.message;
+            document.getElementById('data').innerText = 'No se pudieron obtener los datos: ' + error.message;
         });
 }
